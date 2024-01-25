8df740a2ae6c3a5dbdda33a6a9f37ed31a1bcdd9
Gkits-Gk <133623589+Gkits-Gk@users.noreply.github.com>
2024-01-25 12:10:11 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+    public static void main(String []args)
+    {
+       int x=8;
+       int y=1;
+       int result = x +y; 
+       System.out.println(result);
+       
+    }
+}
